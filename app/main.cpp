a99f3a3f373343122eb1f4eece7f6c17c2c180bf
Sukoon Sarin <sukoonsarin@gmail.com>
2020-11-03 15:44:08 -0500

    Combining and fixing minor errors [Task 2.8]

--- app/main.cpp
+++ app/main.cpp
@@ -7,29 +7,43 @@
  * @brief Main file to run detetction and tracking
  */
 #include "../include/DataLoader.h"
-#include "../include/Detection.h"
-#include "../include/Track.h"
+
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+// keys It is used for showing parsing examples.
+const char* keys =
+"{help h usage ? | | Usage examples: \n\t\t./object_detection_yolo.out --image=dog.jpg \n\t\t./object_detection_yolo.out --video=run_sm.mp4}"
+"{image i        |<none>| input image   }"
+"{video v       |<none>| input video   }"
+;
 /**
  * @fn main
  * @brief Main function
  * @detail Finds detection and tracks human, return coordinates in UAV's camera frame
  * @return Program execution status
  */
-int main() {
-    DataLoader data;
-    data.setInputStreamMethod(std::string("Image"));
-    std::cout << "Data input method is " << data.getInputStreamMethod()<< std::endl;
-    cv::imshow("Data",data.getFrame());
+int main(int argc, char** argv) {
     
-    Detection detect;
-    detect.initializeParams(0.8,0.9,320,320);
-   
-    Track track;
-    std::vector<float> v(4);
-    v = { 34.0,23.1,64.2,53.4 };
-    track.initializeParams(v);
-    std::vector<float> v1 = track.getCoordinatesInCameraFrame(v);
+    DataLoader data;
+
+
+    cv::CommandLineParser parser(argc, argv, keys);
+    parser.about("Use this script to run Human detection using YOLOv4 in OpenCV.");
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
+
+    int input_type = data.checkParser(parser);
+    if (input_type == 0)
+        data.processInput(parser);
+     else {
+        data.setInputStreamMethod(std::string("Image"));
+        std::cout << "Data input method is : " << data.getInputStreamMethod()<< std::endl;
+    }
 
   return 0;
 }
