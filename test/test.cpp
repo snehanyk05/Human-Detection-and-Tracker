db4481a085f20e1ceb04ee859b1272b8f871bb69
Sukoon Sarin <sukoonsarin@gmail.com>
2020-11-03 22:19:33 -0500

    Updated Unit Testing for DataLoader, Detection and Tracker Modules [Task 2.9]

--- test/test.cpp
+++ test/test.cpp
@@ -23,59 +23,204 @@
 #include <vector>
 #include <iostream>
 
-// DataLoader dummydataloader;
-// Detection detection1;
-// Track dummytrack;
-// std::vector<float> vec1 = { 0.7, 0.9};
-// // std::vector<std::vector<float>> vec2 = { { 34.0,23.1,64.2,53.4 }, { 64.3,53.3,94.3,83.3 }, { 1.0, 2.0, 3.0, 4.0 } };
-// std::vector<std::vector<float>> vec2 = { { 34.0,23.1,64.2,53.4 }, { 64.3,53.3,94.3,83.3 }};
-// std::map<std::string, std::vector<std::vector<float>>> dic1 =  {
-//     { std::string("A"), vec2},
-//     { std::string("B"), vec2},
-// };
+// keys It is used for showing parsing examples.
+const char *keys =
+    "{help h usage ? | | Usage examples: \n\t\t."
+    "/object_detection_yolo.out --image=dog.jpg \n\t\t."
+    "/object_detection_yolo.out --video=run_sm.mp4}"
+    "{image i        |<none>| input image   }"
+    "{video v       |<none>| input video   }";
+
+DataLoader dummydataloader;
+Detection detection1;
+Track dummytrack;
+
+std::vector<float> vec1 = {0.7, 0.9};
+std::vector<std::vector<float>> vec2 = {{34.0, 23.1, 64.2, 53.4}, {64.3, 53.3, 94.3, 83.3}};
+
+//*****************************************************************************************************
+
+/**
+ * @brief Test case for checkParser method of DataLoader class.
+ */
+TEST(DataLoaderTest, checkParser)
+{
+    int argc = 0;
+    const char *argv = "image";
+    cv::CommandLineParser parser(argc, &argv, keys);
+    EXPECT_EQ(dummydataloader.checkParser(parser), -1);
+}
+
+/**
+ * @brief Test case for getInputStreamMethod and setInputStreamMethod method of DataLoader class. 
+ */
+TEST(DataLoaderTest, SetandgetPath)
+{
+    std::string test_method = "video";
+    dummydataloader.setInputStreamMethod(test_method);
+    EXPECT_EQ(test_method, dummydataloader.getInputStreamMethod());
+}
+
+/**
+ * @brief Test case for processInput method of DatALoader class, in case of input method as image. The
+ * test verifies that processInputo doesnt throw an exception.
+ */
+TEST(DataLoaderTest, checkProcessImage)
+{
+    int argc = 0;
+    const char *argv = "image";
+    cv::CommandLineParser parser(argc, &argv, keys);
+    EXPECT_NO_THROW({
+        dummydataloader.processInput(parser);
+    });
+    EXPECT_NO_FATAL_FAILURE({
+        dummydataloader.processInput(parser);
+    });
+}
+/**
+ * @brief Test case for processInput method of DataLoader class in case of input method as video. The
+ * test verifies that processInput doesnt throw an exception.
+ */
+TEST(DataLoaderTest, checkProcessVideo)
+{
+    int argc = 0;
+    const char *argv = "video";
+    cv::CommandLineParser parser(argc, &argv, keys);
+    DataLoader dummydataloader1("../run.mp4", "video");
+    EXPECT_NO_THROW({
+        dummydataloader.processInput(parser);
+    });
+    EXPECT_NO_FATAL_FAILURE({
+        dummydataloader.processInput(parser);
+    });
+}
+//***************************************************************************************8
+/**
+ * @brief Test case for loadModelandLabelClasses method of Detection class. Loads model class, config and weight files.
+ */
+TEST(DetectionTest, checkModelFiles)
+{
+    std::string dummyclassfile = "../coco.names";
+    std::string dummyweightfile = "../yolov4.weights";
+    std::string dummyconfigfile = "../yolov4.cfg";
+    EXPECT_NO_FATAL_FAILURE({
+        detection1.loadModelandLabelClasses(dummyclassfile, dummyweightfile, dummyconfigfile);
+    });
+}
+/**
+ * @brief Test Case for Initializing Confidence and non maximum suppression threshold along with width and height of the image
+ */
+TEST(DetectionTest, checkInitialParams)
+{
+    EXPECT_NO_FATAL_FAILURE({
+        detection1.initializeParams(0.4, 0.5, 416, 416);
+    });
+}
+/**
+ * @brief Test case for settFrame method of DataLoader class. 
+ */
+TEST(DetectionTest, TestFrame)
+{
+    cv::Mat test_frame = cv::imread("../person.jpg");
+    EXPECT_NO_FATAL_FAILURE({
+        detection1.setFrame(test_frame);
+    });
+}
+
+/**
+ * @brief Test case for processFrameforHuman method. Also checks for Detections return from this method.
+ */
+TEST(DetectionTest, ProcessHumandetections)
+{
+    cv::VideoCapture capture;
+    capture.open("../person.jpg");
+    cv::Mat frame;
+    capture >> frame;
+    Detection detection2;
+    detection2.setFrame(frame);
+    std::vector<cv::Rect> testdetections = detection2.processFrameforHuman();
+
+    std::vector<cv::Rect> getdetect = detection2.getDetections();
+    std::vector<int> testdetect = {testdetections[0].x, testdetections[0].y, testdetections[0].height, testdetections[0].width};
+    std::vector<int> testdetect1 = {getdetect[0].x, getdetect[0].y, getdetect[0].height, getdetect[0].width};
+
+    EXPECT_EQ(testdetect, testdetect1);
+}
+
+/**
+ * @brief Test case for getConfidence method. Checks for any fatal error.
+ */
+TEST(DetectionTest, Processconfidence)
+{
+    cv::VideoCapture capture;
+    capture.open("../person.jpg");
+    cv::Mat frame;
+    capture >> frame;
+    Detection detection2;
+    detection2.setFrame(frame);
+    std::vector<cv::Rect> testdetections2 = detection2.processFrameforHuman();
+
+    std::vector<float> getconf1 = detection2.getConfidence();
+    std::vector<float> getconf2 = detection2.getConfidence();
+    EXPECT_EQ(getconf1, getconf2);
+}
+
+//*************************************************************************************//
+
+/**
+ * @brief Test case for setFrame method of Track class. 
+ */
+TEST(TrackerTest, TestFrame)
+{
+    cv::Mat test_frame = cv::imread("../person.jpg");
+    EXPECT_NO_FATAL_FAILURE({
+        dummytrack.setFrame(test_frame);
+    });
+}
+
+/**
+ * @brief Test case for initialize tracker method of Track class. Checks for any fatal error
+ */
+TEST(TrackerTest, Initializetracker)
+{
+    EXPECT_NO_FATAL_FAILURE({
+        dummytrack.initializeTracker();
+    });
+}
 
 // /**
-//  * @brief Test case for getAllDetectionsList method of Detection class. 
+//  * @brief Test case for runTrackerAlgorithm method of Track class. Checks for any fatal error
 //  */
-// TEST(DetectionTest, checkdetectionlist) {
-//   std::map<std::string, std::vector<std::vector<float>>> testmap = detection1.getAllDetectionsList();
-//   EXPECT_EQ(dic1["A"], testmap.begin()->second);
+// TEST(TrackerTest, runtrackertest)
+// {
+//     cv::VideoCapture capture;
+//     capture.open("../run.mp4");
+//     cv::Mat frame;
+//     capture >> frame;
+//     detection1.setFrame(frame);
+
+//     std::vector<cv::Rect> testdetections = detection1.processFrameforHuman();
+//     std::vector<float> testconfidence = {0.7, 0.9};
+//     dummytrack.initializeTracker();
+//     dummytrack.setFrame(frame);
+
+//     EXPECT_NO_FATAL_FAILURE({
+//         dummytrack.runTrackerAlgorithm(testdetections, testconfidence);
+//     });
 // }
-
 // /**
-//  * @brief Test case for getDetections method of Detection class. 
+//  * @brief Test case for drawGreenBoundingBox method of Track class. checks for exception error.
 //  */
-// TEST(DetectionTest, checkdetections) {;
-//   EXPECT_EQ(vec2, detection1.getDetections() );
-// }
-
-// /**
-//  * @brief Test case for getConfidence method of Detection class. 
-//  */
-// TEST(DetectionTest, checkconfidence) {;
-//   EXPECT_EQ(vec1, detection1.getConfidence() );
-// }
-
-// /**
-//  * @brief Test case for getInputStreamMethod and setInputStreamMethod method of DataLoader class. 
-//  */
-// TEST(DataLoaderTest, SetandgetPath) {
-//   std::string test_method = "Video"; 
-//   dummydataloader.setInputStreamMethod(test_method);
-//   EXPECT_EQ(test_method, dummydataloader.getInputStreamMethod() );
-// }
-
-// /**
-//  * @brief Test case for getFrame method of DataLoaderO class. 
-//  */
-// // TEST(DataLoaderTest, GetFrame) {
-// //   cv::Mat test_frame = cv::imread("../sample.jpg");
-// //   EXPECT_EQ(test_frame, dummydataloader.getFrame() );
-// // }
-
-// /**
-//  * @brief Test case for getCoordinatesInCameraFrame method of Track class. 
-//  */
-// TEST(TrackerTest, Gettrackedcoordinates) {
-//   EXPECT_EQ(vec2[1], dummytrack.getCoordinatesInCameraFrame(vec1) );
+// TEST(TrackerTest, checkgreenboundingbox)
+// {
+//     cv::Mat frame = cv::imread("../person.jpg");
+//     detection1.setFrame(frame);
+//     std::vector<cv::Rect> testdetections = detection1.processFrameforHuman();
+//     std::vector<float> testconfidence = {0.7, 0.9};
+//     dummytrack.setFrame(frame);
+//     dummytrack.initializeTracker();
+//     dummytrack.runTrackerAlgorithm(testdetections, testconfidence);
+//     EXPECT_NO_FATAL_FAILURE({
+//         dummytrack.drawGreenBoundingBox();
+//     });
 // }
